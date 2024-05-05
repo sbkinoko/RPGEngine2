a13100e3f742770a9c17fe4c61cc318bf89b5315
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-05-05 21:26:58 +0900

    タッチした位置の座標を取得

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,12 +1,45 @@
+
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    var x: Float by remember { mutableStateOf(0f) }
+    var y: Float by remember { mutableStateOf(0f) }
 
+    MaterialTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    detectDragGestures(
+                        onDrag = { change, _ ->
+                            x = change.position.x
+                            y = change.position.y
+                        }
+                    )
+                },
+            contentAlignment = Alignment.Center,
+        ) {
+            Column {
+                Text("X:$x")
+                Text("Y:$y")
+            }
+        }
     }
 }
