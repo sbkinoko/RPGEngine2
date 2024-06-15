18c7a2ba0bf01564cdf6691a9ad64eb6f5c52c11
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-06-15 17:34:47 +0900

    hp mpを表示

--- composeApp/src/commonMain/kotlin/layout/battle/status/StatusComponent.kt
+++ composeApp/src/commonMain/kotlin/layout/battle/status/StatusComponent.kt
@@ -1,6 +1,7 @@
 package layout.battle.status
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -8,7 +9,21 @@ import androidx.compose.ui.Modifier
 fun StatusComponent(
     modifier: Modifier = Modifier,
 ) {
-    Column(modifier = modifier) {
+    val hp = 10
+    val maxHp = 100
 
+    val mp = 5
+    val maxMp = 50
+
+    Column(modifier = modifier) {
+        Text(
+            text = "player"
+        )
+        Text(
+            text = "HP $hp/$maxHp"
+        )
+        Text(
+            text = "MP $mp/$maxMp"
+        )
     }
 }
