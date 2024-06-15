46122deb165add7afbe3d73184e91776eb6d443a
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-06-15 17:20:53 +0900

    バトル画面の大枠作成

---
+++ composeApp/src/commonMain/kotlin/layout/battle/BattleScreen.kt
@@ -0,0 +1,49 @@
+package layout.battle
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import values.Colors
+
+@Composable
+fun BattleScreen(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        StatusArea(
+            modifier = Modifier.weight(1f)
+                .fillMaxWidth()
+                .border(
+                    width = 1.dp,
+                    color = Colors.StatusArea,
+                    shape = RectangleShape,
+                ),
+        )
+
+        MonsterArea(
+            modifier = Modifier.weight(1f)
+                .fillMaxWidth()
+                .border(
+                    width = 1.dp,
+                    color = Colors.MonsterArea,
+                    shape = RectangleShape,
+                ),
+        )
+
+        CommandArea(
+            modifier = Modifier.weight(1f)
+                .fillMaxWidth()
+                .border(
+                    width = 1.dp,
+                    color = Colors.BattleCommand,
+                    shape = RectangleShape,
+                ),
+        )
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/layout/battle/CommandArea.kt
@@ -0,0 +1,16 @@
+package layout.battle
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CommandArea(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier,
+    ) {
+
+    }
+}
