fc14ccc3eba30d587da7e3baf43f611626d249b3
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-06-21 21:39:58 +0900

    testのpackageの位置を修正

---
+++ composeApp/src/commonTest/kotlin/common/status/MonsterStatusTest.kt
@@ -0,0 +1,30 @@
+package common.status
+
+import common.status.param.HP
+import common.status.param.MP
+
+class MonsterStatusTest {
+    companion object {
+        private const val MAX_HP = 10
+        private const val MAX_MP = 10
+
+        fun getMonster() = MonsterStatus(
+            imgId = 1,
+            name = "テスト",
+            hp = HP(
+                maxValue = MAX_HP,
+            ),
+            mp = MP(
+                maxValue = MAX_MP,
+            )
+        )
+
+        // HPが0のモンスターを作成
+        fun getNotActiveMonster() = getMonster().copy(
+            hp = HP(
+                maxValue = MAX_MP,
+                value = 0,
+            )
+        )
+    }
+}
---
+++ composeApp/src/commonTest/kotlin/map/domain/MapDataTest.kt
@@ -0,0 +1,82 @@
+package map.domain
+
+import map.data.LoopMap
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MapDataTest {
+
+    private val mapData = LoopMap()
+
+    @Test
+    fun testGetXY() {
+        mapData.getDataAt(
+            x = 0,
+            y = 0,
+        ).apply {
+            assertEquals(
+                1,
+                this,
+            )
+        }
+
+        mapData.getDataAt(
+            x = 0,
+            y = 9,
+        ).apply {
+            assertEquals(
+                2,
+                this,
+            )
+        }
+
+        mapData.getDataAt(
+            x = 9,
+            y = 9,
+        ).apply {
+            assertEquals(
+                1,
+                this,
+            )
+        }
+    }
+
+    @Test
+    fun testGetMapPoint() {
+        mapData.getDataAt(
+            MapPoint(
+                x = 0,
+                y = 0,
+            )
+        ).apply {
+            assertEquals(
+                1,
+                this,
+            )
+        }
+
+        mapData.getDataAt(
+            MapPoint(
+                x = 0,
+                y = 9,
+            )
+        ).apply {
+            assertEquals(
+                2,
+                this,
+            )
+        }
+
+        mapData.getDataAt(
+            MapPoint(
+                x = 9,
+                y = 9,
+            )
+        ).apply {
+            assertEquals(
+                1,
+                this,
+            )
+        }
+    }
+}
