daf804443b44a236e79e173af2d824ec3243f5e2
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-06-22 23:34:07 +0900

    テスト通るようにした

--- composeApp/src/commonMain/kotlin/battle/domain/ActionData.kt
+++ composeApp/src/commonMain/kotlin/battle/domain/ActionData.kt
@@ -1,6 +1,5 @@
 package battle.domain
 
 data class ActionData(
-    val id: Int,
     val target: Int,
 )
--- composeApp/src/commonMain/kotlin/battle/repositoryimpl/ActionRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/battle/repositoryimpl/ActionRepositoryImpl.kt
@@ -4,13 +4,16 @@ import battle.domain.ActionData
 import battle.repository.ActionRepository
 
 class ActionRepositoryImpl : ActionRepository {
-    val actionMap: Map<Int, ActionData> = emptyMap()
+    private val actionMap: MutableMap<Int, ActionData> = mutableMapOf()
 
-    override fun setAction(playerId: Int, target: Int) {
-        TODO("Not yet implemented")
+    override fun setAction(
+        playerId: Int,
+        target: Int,
+    ) {
+        actionMap[playerId] = ActionData(target)
     }
 
     override fun getAction(playerId: Int): ActionData {
-        TODO("Not yet implemented")
+        return actionMap[playerId] ?: ActionData(0)
     }
 }
