749cd97644fd606c28cf32a14885de873856aef7
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-07-10 16:45:36 +0900

    mapDataを追加

--- composeApp/src/commonMain/kotlin/map/repository/backgroundcell/BackgroundRepository.kt
+++ composeApp/src/commonMain/kotlin/map/repository/backgroundcell/BackgroundRepository.kt
@@ -2,11 +2,14 @@ package map.repository.backgroundcell
 
 import kotlinx.coroutines.flow.MutableSharedFlow
 import map.domain.BackgroundCell
+import map.domain.MapData
 
 interface BackgroundRepository {
     val backgroundFlow: MutableSharedFlow<List<List<BackgroundCell>>>
 
     var background: List<List<BackgroundCell>>
+
+    var mapData: MapData
 
     fun getBackgroundAt(x: Int, y: Int): BackgroundCell
 
