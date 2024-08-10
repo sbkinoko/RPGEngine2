b40a1357437aef54d06a7bb17cbcb7b713a2a49d
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-08-10 17:55:36 +0900

    ステータス詳細表示

--- composeApp/src/commonMain/kotlin/menu/layout/StatusMenu.kt
+++ composeApp/src/commonMain/kotlin/menu/layout/StatusMenu.kt
@@ -4,7 +4,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import common.layout.CenterText
 import common.repository.PlayerRepository
@@ -16,6 +21,9 @@ fun StatusMenu(
 ) {
     val repository: PlayerRepository = PlayerRepositoryImpl()
 
+    var status by remember {
+        mutableStateOf(repository.getPlayer(0))
+    }
     Row(modifier = modifier) {
         Column(
             modifier = Modifier
@@ -30,6 +38,15 @@ fun StatusMenu(
                     text = repository.getPlayer(i).name,
                 )
             }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+        ) {
+            Text(status.name)
+            Text("HP : ${status.hp.value}/${status.hp.maxValue}")
+            Text("MP : ${status.mp.value}/${status.mp.maxValue}")
         }
     }
 }
