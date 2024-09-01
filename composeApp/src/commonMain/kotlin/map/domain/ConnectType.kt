1ed4f4d2273507d4e2c3498c3f39203ef5dcd204
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-09-01 15:04:00 +0900

    道の画像配置

---
+++ composeApp/src/commonMain/kotlin/map/domain/ConnectType.kt
@@ -0,0 +1,15 @@
+package map.domain
+
+enum class ConnectType {
+    Horizontal,
+    Vertical,
+    Cross,
+    LeftTopUp,
+    LeftTopDown,
+    RightToUp,
+    RightToDown,
+    WithoutLeft,
+    WithoutRight,
+    WithoutUp,
+    WithoutDown,
+}
