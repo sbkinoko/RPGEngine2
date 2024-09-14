a43b6f67579470933a9377b98e3cd38f17ad39a4
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-09-14 15:50:17 +0900

    味方を選べるようにした

--- composeApp/src/commonMain/kotlin/battle/command/selectally/SelectAllyViewModel.kt
+++ composeApp/src/commonMain/kotlin/battle/command/selectally/SelectAllyViewModel.kt
@@ -5,14 +5,30 @@ import battle.domain.CommandType
 import battle.domain.SelectAllyCommand
 import battle.usecase.changeselectingactionplayer.ChangeSelectingActionPlayerUseCase
 import common.values.playerNum
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import menu.domain.SelectManager
 import org.koin.core.component.inject
 
 class SelectAllyViewModel : BattleChildViewModel() {
     private val changeSelectingActionPlayerUseCase: ChangeSelectingActionPlayerUseCase by inject()
 
+    private val _isAllySelecting: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val isAllySelecting = _isAllySelecting.asStateFlow()
+
     override val canBack: Boolean
         get() = true
+
+    init {
+        CoroutineScope(Dispatchers.Default).launch {
+            commandStateRepository.commandTypeFlow.collect {
+                _isAllySelecting.value = it is SelectAllyCommand
+            }
+        }
+    }
 
     override fun isBoundedImpl(commandType: CommandType): Boolean {
         return commandType is SelectAllyCommand
