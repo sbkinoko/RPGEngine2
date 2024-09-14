d031160537ef52b2e8b00ce7a74ec4105b190dfe
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-09-14 15:58:14 +0900

    味方を選べるようにした

--- composeApp/src/commonMain/kotlin/battle/command/selectally/SelectAllyViewModel.kt
+++ composeApp/src/commonMain/kotlin/battle/command/selectally/SelectAllyViewModel.kt
@@ -3,6 +3,7 @@ package battle.command.selectally
 import battle.BattleChildViewModel
 import battle.domain.CommandType
 import battle.domain.SelectAllyCommand
+import battle.repository.action.ActionRepository
 import battle.usecase.changeselectingactionplayer.ChangeSelectingActionPlayerUseCase
 import common.values.playerNum
 import kotlinx.coroutines.CoroutineScope
@@ -15,6 +16,13 @@ import org.koin.core.component.inject
 
 class SelectAllyViewModel : BattleChildViewModel() {
     private val changeSelectingActionPlayerUseCase: ChangeSelectingActionPlayerUseCase by inject()
+    private val actionRepository: ActionRepository by inject()
+
+    private val playerId: Int
+        get() {
+            val command = commandStateRepository.nowCommandType as SelectAllyCommand
+            return command.playerId
+        }
 
     private val _isAllySelecting: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val isAllySelecting = _isAllySelecting.asStateFlow()
@@ -26,6 +34,9 @@ class SelectAllyViewModel : BattleChildViewModel() {
         CoroutineScope(Dispatchers.Default).launch {
             commandStateRepository.commandTypeFlow.collect {
                 _isAllySelecting.value = it is SelectAllyCommand
+                if (isAllySelecting.value) {
+                    selectManager.selected = actionRepository.getAction(playerId).ally
+                }
             }
         }
     }
@@ -35,6 +46,13 @@ class SelectAllyViewModel : BattleChildViewModel() {
     }
 
     override fun goNextImpl() {
+        // ターゲットを保存
+        actionRepository.setAlly(
+            playerId = playerId,
+            // 表示ようにリストになっていたが、保存する際は先頭だけ保存して、
+            //　実際に攻撃するときに攻撃対象を再計算する
+            allyId = selectManager.selected,
+        )
         changeSelectingActionPlayerUseCase.invoke()
     }
 
--- composeApp/src/commonMain/kotlin/battle/domain/ActionData.kt
+++ composeApp/src/commonMain/kotlin/battle/domain/ActionData.kt
@@ -4,6 +4,7 @@ data class ActionData(
     val thisTurnAction: ActionType = ActionType.Normal,
     val lastSelectedAction: ActionType = ActionType.Normal,
     val target: Int = 0,
+    val ally: Int = 0,
     val skillId: Int? = null,
 )
 
