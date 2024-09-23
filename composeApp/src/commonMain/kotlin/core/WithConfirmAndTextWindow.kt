80cf7000b268eedb659d70dea96f8b50b6c3adce
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-09-23 15:04:45 +0900

    軽度なリファクタ

--- composeApp/src/commonMain/kotlin/core/WithConfirmAndTextWindow.kt
+++ composeApp/src/commonMain/kotlin/core/WithConfirmAndTextWindow.kt
@@ -6,35 +6,46 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import common.values.Colors
+import core.confim.ConfirmViewModel
 import core.confim.ConfirmWindow
+import core.text.TextViewModel
 import core.text.TextWindow
+import org.koin.compose.koinInject
 
 @Composable
 fun WithConfirmAndTextWindow(
     confirmCallBack: () -> Unit,
     textCallBack: () -> Unit,
     modifier: Modifier = Modifier,
+    confirmViewModel: ConfirmViewModel = koinInject(),
+    textViewModel: TextViewModel = koinInject(),
     content: @Composable () -> Unit,
 ) {
     Box(modifier = modifier) {
         content()
 
-        ConfirmWindow(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    color = Colors.OverlayMenu
-                ),
-            callBack = confirmCallBack,
-        )
+        val confirmFlg = confirmViewModel.getShowStateAsState().value
+        if (confirmFlg) {
+            ConfirmWindow(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        color = Colors.OverlayMenu
+                    ),
+                callBack = confirmCallBack,
+            )
+        }
 
-        TextWindow(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    color = Colors.OverlayMenu
-                ),
-            callBack = textCallBack,
-        )
+        val textFlg = textViewModel.getShowStateAsState().value
+        if (textFlg) {
+            TextWindow(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        color = Colors.OverlayMenu
+                    ),
+                callBack = textCallBack,
+            )
+        }
     }
 }
