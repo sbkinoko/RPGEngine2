466845fdf5a6d33d2dd64835da29bbf29bc141e5
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-09-29 14:46:19 +0900

    menuのviewModelの紐づけを移動

--- composeApp/src/commonMain/kotlin/menu/MenuViewModel.kt
+++ composeApp/src/commonMain/kotlin/menu/MenuViewModel.kt
@@ -2,76 +2,36 @@ package menu
 
 import controller.domain.ControllerCallback
 import controller.domain.StickPosition
-import core.confim.ConfirmViewModel
-import core.confim.repository.ConfirmRepository
-import core.text.TextViewModel
-import core.text.repository.TextRepository
 import kotlinx.coroutines.flow.SharedFlow
 import menu.domain.MenuType
-import menu.main.MainMenuViewModel
 import menu.repository.menustate.MenuStateRepository
-import menu.skill.list.SkillListViewModel
-import menu.skill.target.SkillTargetViewModel
-import menu.skill.user.SkillUserViewModel
-import menu.status.StatusViewModel
+import menu.usecase.getviewmodelbycommandtype.GetControllerByCommandTypeUseCase
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class MenuViewModel : KoinComponent, ControllerCallback {
     private val menuStateRepository: MenuStateRepository by inject()
-    private val confirmRepository: ConfirmRepository by inject()
-    private val textRepository: TextRepository by inject()
+
+    private val getControllerByCommandTypeUseCase: GetControllerByCommandTypeUseCase by inject()
 
     val menuType: SharedFlow<MenuType> = menuStateRepository.commandTypeFlow
 
-    private val mainMenuViewModel: MainMenuViewModel by inject()
-    private val statusViewModel: StatusViewModel by inject()
-    private val skillUserViewModel: SkillUserViewModel by inject()
-    private val skillListViewModel: SkillListViewModel by inject()
-    private val skillTargetViewModel: SkillTargetViewModel by inject()
-
-    private val confirmViewModel: ConfirmViewModel by inject()
-    private val textViewModel: TextViewModel by inject()
+    private val childController: ControllerCallback?
+        get() = getControllerByCommandTypeUseCase.invoke()
 
     override fun moveStick(stickPosition: StickPosition) {
-        menuStateRepository.nowCommandType
-            .toViewModel()?.moveStick(
-                stickPosition
-            )
-    }
-
-    // todo 専用クラス作る
-    private fun MenuType.toViewModel(): ControllerCallback? {
-        if (confirmRepository.nowCommandType) {
-            return confirmViewModel
-        }
-
-        if (textRepository.nowCommandType) {
-            return textViewModel
-        }
-
-        return when (this) {
-            MenuType.Main -> mainMenuViewModel
-            MenuType.Status -> statusViewModel
-            MenuType.SKILL_USER -> skillUserViewModel
-            MenuType.SKILL_LST -> skillListViewModel
-            MenuType.SKILL_TARGET -> skillTargetViewModel
-            MenuType.Item3 -> null
-            MenuType.Item4 -> null
-            MenuType.Item5 -> null
-            MenuType.Item6 -> null
-        }
+        childController?.moveStick(stickPosition)
     }
 
     override fun pressA() {
-        menuStateRepository.nowCommandType.toViewModel()?.pressA()
+        childController?.pressA()
     }
 
     override fun pressB() {
-        menuStateRepository.nowCommandType.toViewModel()?.pressB()
+        childController?.pressB()
     }
 
     override fun pressM() {
-        menuStateRepository.nowCommandType.toViewModel()?.pressM()
+        childController?.pressM()
     }
 }
---
+++ composeApp/src/commonMain/kotlin/menu/usecase/getviewmodelbycommandtype/GetControllerByCommandTypeUseCaseImpl.kt
@@ -0,0 +1,53 @@
+package menu.usecase.getviewmodelbycommandtype
+
+import controller.domain.ControllerCallback
+import core.confim.ConfirmViewModel
+import core.confim.repository.ConfirmRepository
+import core.text.TextViewModel
+import core.text.repository.TextRepository
+import menu.domain.MenuType
+import menu.main.MainMenuViewModel
+import menu.repository.menustate.MenuStateRepository
+import menu.skill.list.SkillListViewModel
+import menu.skill.target.SkillTargetViewModel
+import menu.skill.user.SkillUserViewModel
+import menu.status.StatusViewModel
+
+class GetControllerByCommandTypeUseCaseImpl(
+    private val confirmRepository: ConfirmRepository,
+    private val textRepository: TextRepository,
+    private val menuStateRepository: MenuStateRepository,
+
+    private val mainMenuViewModel: MainMenuViewModel,
+    private val statusViewModel: StatusViewModel,
+    private val skillUserViewModel: SkillUserViewModel,
+    private val skillListViewModel: SkillListViewModel,
+    private val skillTargetViewModel: SkillTargetViewModel,
+
+    private val confirmViewModel: ConfirmViewModel,
+    private val textViewModel: TextViewModel,
+) : GetControllerByCommandTypeUseCase {
+
+
+    override fun invoke(): ControllerCallback? {
+        if (confirmRepository.nowCommandType) {
+            return confirmViewModel
+        }
+
+        if (textRepository.nowCommandType) {
+            return textViewModel
+        }
+
+        return when (menuStateRepository.nowCommandType) {
+            MenuType.Main -> mainMenuViewModel
+            MenuType.Status -> statusViewModel
+            MenuType.SKILL_USER -> skillUserViewModel
+            MenuType.SKILL_LST -> skillListViewModel
+            MenuType.SKILL_TARGET -> skillTargetViewModel
+            MenuType.Item3 -> null
+            MenuType.Item4 -> null
+            MenuType.Item5 -> null
+            MenuType.Item6 -> null
+        }
+    }
+}
