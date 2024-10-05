9117f3f81dadb76381e549739a22a5bc8eb497a3
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-05 17:49:20 +0900

    共通レイアウトを作成

---
+++ composeApp/src/commonMain/kotlin/gamescreen/menu/item/user/UserWindow.kt
@@ -0,0 +1,37 @@
+package gamescreen.menu.item.user
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import common.values.Colors
+
+@Composable
+fun UserWindow(
+    toolUserViewModel: ItemUserViewModel,
+    selectedId: Int,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .background(
+                color = Colors.MenuBackground,
+            )
+    ) {
+        UserList(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+            itemUserViewModel = toolUserViewModel,
+        )
+
+        UnSelectableItemList(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+            selectedUserId = selectedId,
+            itemUserViewModel = toolUserViewModel,
+        )
+    }
+}
