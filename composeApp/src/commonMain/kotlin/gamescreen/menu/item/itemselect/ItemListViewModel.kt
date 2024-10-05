4af00dbf93a94406fcf55bc31f3d3a9b4a96f419
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-05 18:49:26 +0900

    共通要素の修正

--- composeApp/src/commonMain/kotlin/gamescreen/menu/item/itemselect/ItemListViewModel.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/menu/item/itemselect/ItemListViewModel.kt
@@ -1,9 +1,6 @@
 package gamescreen.menu.item.itemselect
 
-import core.domain.AbleType
-import core.domain.Place
 import core.repository.item.ItemRepository
-import core.repository.item.skill.SkillRepository
 import core.repository.player.PlayerRepository
 import core.text.repository.TextRepository
 import core.usecase.checkcanuseskill.CheckCanUseSkillUseCase
@@ -11,8 +8,7 @@ import gamescreen.menu.MenuChildViewModel
 import gamescreen.menu.domain.MenuType
 import gamescreen.menu.domain.SelectManager
 import gamescreen.menu.item.ItemList
-import gamescreen.menu.item.skill.repository.skilluser.SkillUserRepository
-import gamescreen.menu.item.skill.repository.useid.UseSkillIdRepository
+import gamescreen.menu.item.user.UserRepository
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -20,14 +16,12 @@ abstract class ItemListViewModel : MenuChildViewModel(),
     ItemList,
     KoinComponent {
     protected val playerRepository: PlayerRepository by inject()
-    private val skillUserRepository: SkillUserRepository by inject()
-    private val useSkillIdRepository: UseSkillIdRepository by inject()
-    private val skillRepository: SkillRepository by inject()
+    protected abstract val userRepository: UserRepository
+    protected abstract val useItemIdRepository: UseItemIdRepository
     protected abstract val itemRepository: ItemRepository
+    private val textRepository: TextRepository by inject()
 
     private val checkCanUseSkillUseCase: CheckCanUseSkillUseCase by inject()
-
-    private val textRepository: TextRepository by inject()
 
     override var selectManager = SelectManager(
         width = 1,
@@ -37,48 +31,50 @@ abstract class ItemListViewModel : MenuChildViewModel(),
     override val canBack: Boolean
         get() = true
 
-    override fun goNextImpl() {
-        val position = selectManager.selected
-        val skillId = playerRepository.getStatus(userId).skillList[position]
-        val status = playerRepository.getStatus(userId)
+    val userId: Int
+        get() = userRepository.userId
 
-        val ableType = checkCanUseSkillUseCase.invoke(
-            skillId = skillId,
-            status = status,
-            here = Place.MAP,
-        )
-        when (ableType) {
-            AbleType.Able -> {
-                // skillIdを保存
-                useSkillIdRepository.skillId = skillId
-                //　次の画面に遷移
-                commandRepository.push(
-                    MenuType.SKILL_TARGET,
-                )
-            }
-
-            AbleType.CANT_USE_BY_PLACE -> {
-                textRepository.setText("ここでは使えません")
-                textRepository.push(true)
-            }
-
-            AbleType.CANT_USE_BY_MP -> {
-                textRepository.setText("MPがたりません")
-                textRepository.push(true)
-            }
-        }
-    }
+    protected abstract val itemList: List<Int>
 
     protected abstract val boundedScreenType: MenuType
+    protected abstract val nextScreenType: MenuType
+
+//    override fun goNextImpl() {
+//        val position = selectManager.selected
+//        val id = itemList[position]
+//        val status = playerRepository.getStatus(userId)
+//
+//        val ableType = checkCanUseSkillUseCase.invoke(
+//            skillId = id,
+//            status = status,
+//            here = Place.MAP,
+//        )
+//
+//        when (ableType) {
+//            AbleType.Able -> {
+//                // skillIdを保存
+//                useItemIdRepository.itemId = id
+//                //　次の画面に遷移
+//                commandRepository.push(
+//                    nextScreenType,
+//                )
+//            }
+//
+//            AbleType.CANT_USE_BY_PLACE -> {
+//                textRepository.setText("ここでは使えません")
+//                textRepository.push(true)
+//            }
+//
+//            AbleType.CANT_USE_BY_MP -> {
+//                textRepository.setText("MPがたりません")
+//                textRepository.push(true)
+//            }
+//        }
+//    }
 
     override fun isBoundedImpl(commandType: MenuType): Boolean {
         return commandType == boundedScreenType
     }
-
-    val userId: Int
-        get() = skillUserRepository.skillUserId
-
-    protected abstract val itemList: List<Int>
 
     fun init() {
         loadItem()
@@ -94,7 +90,7 @@ abstract class ItemListViewModel : MenuChildViewModel(),
 
     fun getExplainAt(position: Int): String {
         val skillId = playerRepository.getStatus(userId).skillList[position]
-        return skillRepository.getSkill(skillId).explain
+        return itemRepository.getItem(skillId).explain
     }
 
     override fun pressB() {
