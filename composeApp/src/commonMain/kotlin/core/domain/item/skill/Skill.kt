0e1b74175c0dda7cbf3654f4240960877ae29896
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-14 14:55:05 +0900

    共通化のリファクタ

--- composeApp/src/commonMain/kotlin/core/domain/item/skill/Skill.kt
+++ composeApp/src/commonMain/kotlin/core/domain/item/skill/Skill.kt
@@ -2,7 +2,6 @@ package core.domain.item.skill
 
 import core.domain.Place
 import core.domain.item.Item
-import core.domain.item.TargetType
 
 sealed interface Skill : Item {
     val needMP: Int
@@ -20,23 +19,6 @@ data class AttackSkill(
     override val canUse: (Int) -> Boolean = { mp -> mp >= needMP },
     override val usablePlace: Place,
     val damageAmount: Int,
-) : Skill {
-    override val explain: String
-        get() {
-            return name + "\n" +
-                    "${id}番目のスキル"
-        }
-}
-
-data class HealSkill(
-    override val id: Int,
-    override val name: String,
-    override val needMP: Int,
-    override val targetNum: Int,
-    override val canUse: (Int) -> Boolean = { mp -> mp >= needMP },
-    override val usablePlace: Place,
-    val healAmount: Int,
-    val targetType: TargetType,
 ) : Skill {
     override val explain: String
         get() {
