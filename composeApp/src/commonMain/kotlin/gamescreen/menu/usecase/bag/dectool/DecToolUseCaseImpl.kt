21cc83075ec0f0c9a1057af652b44b8e1082daba
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-27 22:20:17 +0900

    interfaceの作成

---
+++ composeApp/src/commonMain/kotlin/gamescreen/menu/usecase/bag/dectool/DecToolUseCaseImpl.kt
@@ -0,0 +1,7 @@
+package gamescreen.menu.usecase.bag.dectool
+
+class DecToolUseCaseImpl : DecToolUseCase {
+    override fun invoke(itemId: Int, itemNum: Int) {
+        TODO("Not yet implemented")
+    }
+}
