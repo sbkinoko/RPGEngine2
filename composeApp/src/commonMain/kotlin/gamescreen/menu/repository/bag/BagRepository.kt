8b1845262568da2caf573b0b0f00a3e5f4bc3b21
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-27 21:39:16 +0900

    interface修正

--- composeApp/src/commonMain/kotlin/gamescreen/menu/repository/bag/BagRepository.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/menu/repository/bag/BagRepository.kt
@@ -3,15 +3,7 @@ package gamescreen.menu.repository.bag
 import gamescreen.menu.domain.BagToolData
 
 interface BagRepository {
-    fun add(
-        id: Int,
-        num: Int,
-    )
-
-    fun dec(
-        id: Int,
-        num: Int,
-    )
-
     fun getList(): List<BagToolData>
+
+    fun setData(data: BagToolData)
 }
---
+++ composeApp/src/commonMain/kotlin/gamescreen/menu/repository/bag/BagRepositoryImpl.kt
@@ -0,0 +1,14 @@
+package gamescreen.menu.repository.bag
+
+import gamescreen.menu.domain.BagToolData
+
+class BagRepositoryImpl : BagRepository {
+
+    override fun getList(): List<BagToolData> {
+        TODO("Not yet implemented")
+    }
+
+    override fun setData(data: BagToolData) {
+        TODO("Not yet implemented")
+    }
+}
