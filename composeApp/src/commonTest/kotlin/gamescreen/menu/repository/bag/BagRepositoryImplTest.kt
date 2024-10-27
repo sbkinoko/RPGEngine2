d0f6352144ab743e0839367f6c7d49c2f3dc86df
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-27 21:39:29 +0900

    test作成

---
+++ composeApp/src/commonTest/kotlin/gamescreen/menu/repository/bag/BagRepositoryImplTest.kt
@@ -0,0 +1,35 @@
+package gamescreen.menu.repository.bag
+
+import gamescreen.menu.domain.BagToolData
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class BagRepositoryImplTest : KoinTest {
+    private val bagRepository: BagRepository by inject()
+
+    @Test
+    fun setTest() {
+        val data1 = BagToolData(1, 1)
+        bagRepository.setData(data1)
+        assertEquals(
+            actual = bagRepository.getList(),
+            expected = listOf(data1)
+        )
+
+        val data2 = BagToolData(2, 2)
+        bagRepository.setData(data2)
+        assertEquals(
+            actual = bagRepository.getList(),
+            expected = listOf(data1, data2),
+        )
+
+        val data3 = BagToolData(1, 3)
+        bagRepository.setData(data3)
+        assertEquals(
+            actual = bagRepository.getList(),
+            expected = listOf(data3, data2),
+        )
+    }
+}
