3c74fbeb008f8f0350dee03b1c4a31c6d81ad3c4
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-10-27 22:25:31 +0900

    test作成

---
+++ composeApp/src/commonTest/kotlin/gamescreen/menu/usecase/bag/dectool/DecToolRepositoryImplTest.kt
@@ -0,0 +1,70 @@
+package gamescreen.menu.usecase.bag.dectool
+
+import gamescreen.menu.MenuModule
+import gamescreen.menu.domain.BagToolData
+import gamescreen.menu.repository.bag.BagRepository
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DecToolUseCaseImplTest : KoinTest {
+    private val bagRepository: BagRepository by inject()
+    private val decToolUseCase: DecToolUseCase by inject()
+
+    @BeforeTest
+    fun beforeTest() {
+        startKoin {
+            modules(
+                MenuModule,
+            )
+        }
+    }
+
+    @AfterTest
+    fun afterTest() {
+        stopKoin()
+    }
+
+    @Test
+    fun testDecTool() {
+        val id = 1
+        val num = 1
+        val bagToolData = BagToolData(
+            id = id,
+            num = num,
+        )
+        bagRepository.setData(
+            data = bagToolData,
+        )
+
+        decToolUseCase(
+            itemId = id,
+            itemNum = num,
+        )
+
+        assertEquals(
+            expected = 0,
+            actual = bagRepository.getList().find { it.id == 1 }?.num,
+        )
+
+        var count = 0
+        try {
+            decToolUseCase(
+                itemId = id,
+                itemNum = num,
+            )
+        } catch (e: IllegalStateException) {
+            count++
+        }
+
+        assertEquals(
+            expected = 1,
+            actual = count,
+        )
+    }
+}
