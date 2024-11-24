23777ef0e17c80aa5fbd73a56b47b6f3b90d71ca
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-11-24 15:46:56 +0900

    イベントを起こせるようにした

--- composeApp/src/androidMain/kotlin/org/example/project/preview/PlayerPreview.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/preview/PlayerPreview.kt
@@ -6,8 +6,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import gamescreen.map.domain.PlayerDir
-import gamescreen.map.domain.Point
-import gamescreen.map.domain.collision.Square
 import gamescreen.map.layout.Player
 
 @Preview
@@ -17,14 +15,6 @@ fun PlayerPreview() {
         modifier = Modifier.fillMaxSize()
     ) {
         Player(
-            square = Square(
-                displayPoint = Point(
-                    x = 100f,
-                    y = 100f,
-                ),
-                size = 100f,
-            ),
-            screenRatio = 1f,
             dir = PlayerDir.UP,
         )
     }
