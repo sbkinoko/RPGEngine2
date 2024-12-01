6eb099075547fad4e64148ce9fe5ecec4f661bef
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-12-01 13:39:31 +0900

    戦闘終了画面作成

--- composeApp/src/commonMain/kotlin/gamescreen/battle/command/finish/FinishCommandWindow.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/battle/command/finish/FinishCommandWindow.kt
@@ -1,13 +1,29 @@
 package gamescreen.battle.command.finish
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import gamescreen.battle.BattleViewModel
 import org.koin.compose.koinInject
 
 @Composable
 fun FinishCommandWindow(
+    modifier: Modifier = Modifier,
     battleViewModel: BattleViewModel = koinInject(),
 ) {
-    // fixme 画像がないのでチカチカする
-    battleViewModel.finishBattle()
+    Box(
+        modifier = modifier
+            .clickable {
+                battleViewModel.finishBattle()
+            },
+    ) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.Center),
+            text = "戦闘に勝利した",
+        )
+    }
 }
