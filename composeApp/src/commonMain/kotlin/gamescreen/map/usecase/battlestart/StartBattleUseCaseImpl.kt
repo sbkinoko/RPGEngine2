56353dc905cf59ce06725b44d823db37b14e9409
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-12-29 15:12:04 +0900

    戦闘モンスターを決定するUseCaseを作成

--- composeApp/src/commonMain/kotlin/gamescreen/map/usecase/battlestart/StartBattleUseCaseImpl.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/map/usecase/battlestart/StartBattleUseCaseImpl.kt
@@ -1,34 +1,26 @@
-package gamescreen.map.usecase.startbattle
+package gamescreen.map.usecase.battlestart
 
 import core.domain.ScreenType
+import core.domain.status.MonsterStatus
 import core.repository.battlemonster.BattleMonsterRepository
 import core.repository.screentype.ScreenTypeRepository
-import data.monster.MonsterRepository
 import gamescreen.battle.repository.action.ActionRepository
 import gamescreen.battle.repository.commandstate.CommandStateRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
-import kotlin.random.Random
 
 class StartBattleUseCaseImpl(
     private val battleMonsterRepository: BattleMonsterRepository,
     private val screenTypeRepository: ScreenTypeRepository,
     private val commandStateRepository: CommandStateRepository,
     private val actionRepository: ActionRepository,
-    private val monsterRepository: MonsterRepository,
 ) : StartBattleUseCase, KoinComponent {
 
-    override operator fun invoke() {
-        // fixme マスによって出現モンスターを変える
-        // ランダムで1~5の敵を作成
-        val monsterList = List(
-            Random.nextInt(5) + 1,
-        ) {
-            monsterRepository.getMonster(1)
-        }
-
+    override operator fun invoke(
+        monsterList: List<MonsterStatus>,
+    ) {
         CoroutineScope(Dispatchers.Default).launch {
             battleMonsterRepository.setMonsters(
                 monsterList
--- composeApp/src/commonMain/kotlin/gamescreen/map/usecase/startbattle/StartBattleUseCase.kt
+++
@@ -1,5 +0,0 @@
-package gamescreen.map.usecase.startbattle
-
-interface StartBattleUseCase {
-    operator fun invoke()
-}
