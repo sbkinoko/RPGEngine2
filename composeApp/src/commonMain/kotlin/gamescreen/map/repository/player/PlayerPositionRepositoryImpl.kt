9895af38a0656f7ab7b6bafe0b02e61c52203cb6
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-01-01 00:40:07 +0900

    不要な要素を削除

--- composeApp/src/commonMain/kotlin/gamescreen/map/repository/player/PlayerPositionRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/map/repository/player/PlayerPositionRepositoryImpl.kt
@@ -1,16 +1,11 @@
 package gamescreen.map.repository.player
 
 import gamescreen.map.domain.collision.Square
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class PlayerPositionRepositoryImpl : PlayerPositionRepository {
-    override val playerPositionFLow: MutableSharedFlow<Square> = MutableSharedFlow(replay = 1)
-
-    private var playerPosition: Square = PlayerPositionRepository.initialSquare
-
     private val mutablePlayerPositionStateFlow = MutableStateFlow(
         PlayerPositionRepository.initialSquare
     )
@@ -19,16 +14,11 @@ class PlayerPositionRepositoryImpl : PlayerPositionRepository {
         get() = mutablePlayerPositionStateFlow.asStateFlow()
 
     override fun getPlayerPosition(): Square {
-        return playerPosition
+        return playerPositionStateFlow.value
     }
 
     override suspend fun setPlayerPosition(square: Square) {
-        playerPosition = square
-        playerPositionFLow.emit(square)
         mutablePlayerPositionStateFlow.value = square
     }
 
-    override suspend fun reload() {
-        playerPositionFLow.emit(playerPosition)
-    }
 }
