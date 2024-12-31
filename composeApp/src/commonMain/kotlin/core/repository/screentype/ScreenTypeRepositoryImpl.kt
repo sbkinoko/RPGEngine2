4836a32c0ac5aa07c18deb18bb2efbd2ab29b10d
sbknk <136720653+sbkinoko@users.noreply.github.com>
2024-12-31 20:53:27 +0900

    不要なメンバ削除

--- composeApp/src/commonMain/kotlin/core/repository/screentype/ScreenTypeRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/core/repository/screentype/ScreenTypeRepositoryImpl.kt
@@ -2,16 +2,13 @@ package core.repository.screentype
 
 import core.domain.ScreenType
 import core.repository.screentype.ScreenTypeRepository.Companion.INITIAL_SCREEN_TYPE
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class ScreenTypeRepositoryImpl : ScreenTypeRepository {
-    override val screenTypeFlow: MutableSharedFlow<ScreenType> = MutableSharedFlow(replay = 1)
-
     private val mutableScreenTypeStateFlow =
-        MutableStateFlow<ScreenType>(
+        MutableStateFlow(
             INITIAL_SCREEN_TYPE
         )
 
