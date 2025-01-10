b43d183088f4ed5f72c3422a571d757f23898e81
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-01-10 17:50:46 +0900

    isPlayerIncludeCellをvalに変更

--- composeApp/src/commonMain/kotlin/gamescreen/map/usecase/UpdateCellContainPlayerUseCase.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/map/usecase/UpdateCellContainPlayerUseCase.kt
@@ -4,6 +4,9 @@ import gamescreen.map.domain.BackgroundCell
 import gamescreen.map.repository.backgroundcell.BackgroundRepository
 import gamescreen.map.repository.player.PlayerPositionRepository
 import gamescreen.map.repository.playercell.PlayerCellRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * プレイヤーが入っているセルを更新
@@ -16,17 +19,50 @@ class UpdateCellContainPlayerUseCase(
     operator fun invoke() {
         var playerIncludeCell: BackgroundCell? = null
 
-        backgroundRepository.backgroundStateFlow.value.map { rowArray ->
-            rowArray.map { cell ->
-                cell.apply {
-                    if (playerPositionRepository.getPlayerPosition().isIn(square)) {
-                        isPlayerIncludeCell = true
-                        playerIncludeCell = this
-                    } else {
-                        isPlayerIncludeCell = false
+        // プレイヤーを包含しているセルを保存
+        backgroundRepository
+            .backgroundStateFlow
+            .value
+            .map row@{ rowArray ->
+                rowArray.map { cell ->
+                    if (
+                        playerPositionRepository
+                            .getPlayerPosition()
+                            .isIn(cell.square)
+                    ) {
+                        playerIncludeCell = cell
+                        return@row
                     }
                 }
             }
+
+        //fixme PlayerCellRepositoryImplを修正したら削除する
+        // プレイヤーを包含しているセルを更新する
+        val background = backgroundRepository
+            .backgroundStateFlow
+            .value
+            .map { rowArray ->
+                rowArray.map { cell ->
+                    if (
+                        playerPositionRepository
+                            .getPlayerPosition()
+                            .isIn(cell.square)
+                    ) {
+                        cell.copy(
+                            isPlayerIncludeCell = true,
+                        )
+                    } else {
+                        cell.copy(
+                            isPlayerIncludeCell = false,
+                        )
+                    }
+                }
+            }
+
+        CoroutineScope(Dispatchers.Default).launch {
+            backgroundRepository.setBackground(
+                background = background
+            )
         }
         playerCellRepository.playerIncludeCell = playerIncludeCell
     }
