4743978bff7f120f0f9ad8ca938519eda87f0703
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-01-16 18:57:39 +0900

    買い物画面を表示

--- composeApp/src/commonMain/kotlin/gamescreen/mapshop/ModuleShop.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/mapshop/ModuleShop.kt
@@ -6,6 +6,10 @@ import org.koin.dsl.module
 
 val ModuleShop = module {
 
+    single {
+        ShopViewModel()
+    }
+
     single<ShopMenuRepository> {
         ShopMenuRepositoryImpl()
     }
--- composeApp/src/commonMain/kotlin/gamescreen/mapshop/ShopViewModel.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/mapshop/ShopViewModel.kt
@@ -1,7 +1,19 @@
 package gamescreen.mapshop
 
+import gamescreen.mapshop.repoisitory.ShopMenuRepository
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 class ShopViewModel : KoinComponent {
+
+    private val shopMenuRepository: ShopMenuRepository by inject()
+    val isShopMenuVisibleStateFlow =
+        shopMenuRepository.isVisibleStateFlow
+
+    fun hideMenu() {
+        shopMenuRepository.setVisibility(
+            isVisible = false,
+        )
+    }
 
 }
