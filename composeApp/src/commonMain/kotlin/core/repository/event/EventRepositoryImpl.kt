d19cd4fdd8f66fe1b206ff7a147174eeb938cdea
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-02-02 13:58:05 +0900

    ロジック作成

--- composeApp/src/commonMain/kotlin/core/repository/event/EventRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/core/repository/event/EventRepositoryImpl.kt
@@ -1,20 +1,44 @@
 package core.repository.event
 
 import core.domain.BattleResult
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 class EventRepositoryImpl : EventRepository {
+    private var winEvent = {}
+    private var looseEvent = {}
+
+    private val mutableBattleResultFlow = MutableStateFlow(
+        BattleResult.None,
+    )
     override val resultStateFlow: StateFlow<BattleResult>
-        get() = TODO("Not yet implemented")
+        get() = mutableBattleResultFlow.asStateFlow()
+
+    init {
+        CoroutineScope(Dispatchers.Default).launch {
+            resultStateFlow.collect {
+                when (it) {
+                    BattleResult.Win -> winEvent.invoke()
+                    BattleResult.Lose -> looseEvent.invoke()
+                    BattleResult.None -> Unit
+                }
+            }
+        }
+    }
 
     override fun setResult(result: BattleResult) {
-        TODO("Not yet implemented")
+        mutableBattleResultFlow.value = result
     }
 
     override fun setCallBack(
         winEvent: () -> Unit,
         looseEvent: () -> Unit,
     ) {
-        TODO("Not yet implemented")
+        this.winEvent = winEvent
+        this.looseEvent = looseEvent
     }
 }
