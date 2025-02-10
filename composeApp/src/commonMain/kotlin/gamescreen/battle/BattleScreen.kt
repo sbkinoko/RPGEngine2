b80e5991c061679764ccfae1aa83592c977d10f7
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-02-10 15:35:29 +0900

    戦闘背景追加

--- composeApp/src/commonMain/kotlin/gamescreen/battle/BattleScreen.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/battle/BattleScreen.kt
@@ -1,7 +1,10 @@
 package gamescreen.battle
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -14,9 +17,14 @@ import gamescreen.battle.command.CommandArea
 import gamescreen.battle.monster.MonsterArea
 import gamescreen.battle.status.StatusArea
 import kotlinx.coroutines.delay
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.koinInject
+import rpgengine.composeapp.generated.resources.Res
+import rpgengine.composeapp.generated.resources.bt_bg_01
 import values.Colors
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun BattleScreen(
     modifier: Modifier = Modifier,
@@ -37,42 +45,53 @@ fun BattleScreen(
         }
     }
 
-    Column(
+    Box(
         modifier = modifier,
     ) {
-        StatusArea(
-            modifier = Modifier.weight(1f)
-                .fillMaxWidth()
-                .border(
-                    width = 1.dp,
-                    color = Colors.StatusArea,
-                    shape = RectangleShape,
-                ),
-            statusList = playerStatusList.value,
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(
+                Res.drawable.bt_bg_01
+            ),
+            contentDescription = "戦闘背景"
         )
-
-        battleViewModel.apply {
-            MonsterArea(
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            StatusArea(
                 modifier = Modifier.weight(1f)
                     .fillMaxWidth()
                     .border(
                         width = 1.dp,
-                        color = Colors.MonsterArea,
+                        color = Colors.StatusArea,
                         shape = RectangleShape,
                     ),
-                monsters = monsters,
+                statusList = playerStatusList.value,
+            )
+
+            battleViewModel.apply {
+                MonsterArea(
+                    modifier = Modifier.weight(1f)
+                        .fillMaxWidth()
+                        .border(
+                            width = 1.dp,
+                            color = Colors.MonsterArea,
+                            shape = RectangleShape,
+                        ),
+                    monsters = monsters,
+                )
+            }
+
+            CommandArea(
+                modifier = Modifier.weight(1f)
+                    .fillMaxWidth()
+                    .border(
+                        width = 1.dp,
+                        color = Colors.BattleCommand,
+                        shape = RectangleShape,
+                    ),
+                battleViewModel = battleViewModel,
             )
         }
-
-        CommandArea(
-            modifier = Modifier.weight(1f)
-                .fillMaxWidth()
-                .border(
-                    width = 1.dp,
-                    color = Colors.BattleCommand,
-                    shape = RectangleShape,
-                ),
-            battleViewModel = battleViewModel,
-        )
     }
 }
