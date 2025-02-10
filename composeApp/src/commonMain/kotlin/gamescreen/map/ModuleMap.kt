5ecdb466ed7ed7ce01bc7a877519f9368972640d
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-02-10 17:09:18 +0900

    通常戦を始めるuseCase作成

--- composeApp/src/commonMain/kotlin/gamescreen/map/ModuleMap.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/map/ModuleMap.kt
@@ -23,6 +23,8 @@ import gamescreen.map.usecase.battledecidemonster.DecideBattleMonsterUseCase
 import gamescreen.map.usecase.battledecidemonster.DecideBattleMonsterUseCaseImpl
 import gamescreen.map.usecase.battleevent.StartEventBattleUseCase
 import gamescreen.map.usecase.battleevent.StartEventBattleUseCaseImpl
+import gamescreen.map.usecase.battlenormal.StartNormalBattleUseCase
+import gamescreen.map.usecase.battlenormal.StartNormalBattleUseCaseImpl
 import gamescreen.map.usecase.battlestart.StartBattleUseCase
 import gamescreen.map.usecase.battlestart.StartBattleUseCaseImpl
 import gamescreen.map.usecase.collision.geteventtype.GetEventTypeUseCase
@@ -211,6 +213,16 @@ val ModuleMap = module {
         StartEventBattleUseCaseImpl(
             battleDataRepository = get(),
             startBattleUseCase = get(),
+        )
+    }
+
+    single<StartNormalBattleUseCase> {
+        StartNormalBattleUseCaseImpl(
+            playerCellRepository = get(),
+            decideBattleMonsterUseCase = get(),
+            startBattleUseCase = get(),
+            textRepository = get(),
+            restartUseCase = get(),
         )
     }
 
---
+++ composeApp/src/commonMain/kotlin/gamescreen/map/usecase/battlenormal/StartNormalBattleUseCase.kt
@@ -0,0 +1,5 @@
+package gamescreen.map.usecase.battlenormal
+
+interface StartNormalBattleUseCase {
+    operator fun invoke()
+}
