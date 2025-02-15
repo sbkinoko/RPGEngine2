d23b8cf0f6120bc737a19e5a516c8643be5b29cf
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-02-15 22:00:15 +0900

    確率で麻痺が治るようにした

--- composeApp/src/commonMain/kotlin/core/domain/status/ConditionType.kt
+++ composeApp/src/commonMain/kotlin/core/domain/status/ConditionType.kt
@@ -1,8 +1,24 @@
 package core.domain.status
 
+import kotlin.random.Random
+
 sealed class ConditionType {
     class Paralysis(
         val probability: Int = 50,
         val cure: Int = 50,
     ) : ConditionType()
+}
+
+/**
+ * 確率によって治った麻痺を除去したリストを作成
+ */
+fun List<ConditionType>.tryCureParalyze(): List<ConditionType> = this.filter {
+    // 麻痺以外は持ってくる
+    if (it !is ConditionType.Paralysis) {
+        return@filter true
+    }
+
+    val rand = Random.nextInt(100)
+    // 治るなら除去
+    rand >= it.cure
 }
