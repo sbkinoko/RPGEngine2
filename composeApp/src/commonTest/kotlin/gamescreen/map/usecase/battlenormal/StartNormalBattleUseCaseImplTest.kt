5215b37cd17682befb33b8f0c8e1a6fb58f8add1
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-02-21 19:44:43 +0900

    通常戦闘のテスト作成

---
+++ composeApp/src/commonTest/kotlin/gamescreen/map/usecase/battlenormal/StartNormalBattleUseCaseImplTest.kt
@@ -0,0 +1,221 @@
+package gamescreen.map.usecase.battlenormal
+
+import core.domain.BattleEventCallback
+import core.domain.mapcell.CellType
+import core.domain.mapcell.toBattleBackGround
+import core.domain.status.MonsterStatusTest.Companion.TestActiveMonster
+import core.domain.status.monster.MonsterStatus
+import core.usecase.restart.RestartUseCase
+import gamescreen.battle.domain.BattleBackgroundType
+import gamescreen.map.domain.MapPoint
+import gamescreen.map.domain.background.BackgroundCell
+import gamescreen.map.domain.collision.square.NormalSquare
+import gamescreen.map.repository.playercell.PlayerCellRepository
+import gamescreen.map.usecase.battledecidemonster.DecideBattleMonsterUseCase
+import gamescreen.map.usecase.battlestart.StartBattleUseCase
+import gamescreen.text.TextBoxData
+import gamescreen.text.repository.TextRepository
+import kotlinx.coroutines.flow.StateFlow
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class StartNormalBattleUseCaseImplTest {
+    private val textRepository = object : TextRepository {
+        override val callBack: (() -> Unit)
+            get() = throw NotImplementedError()
+        override val text: String
+            get() = throw NotImplementedError()
+        override val textDataStateFlow: StateFlow<TextBoxData?>
+            get() = throw NotImplementedError()
+        override val nowTextData: TextBoxData
+            get() = throw NotImplementedError()
+
+        override fun push(textBoxData: TextBoxData) {
+            throw NotImplementedError()
+        }
+
+        override fun push(textBoxDataList: List<TextBoxData>) {
+            throw NotImplementedError()
+        }
+
+        override fun pop() {
+            throw NotImplementedError()
+        }
+    }
+
+    private val restartUseCase = object : RestartUseCase {
+        override suspend fun invoke() {
+            throw NotImplementedError()
+        }
+    }
+
+    lateinit var decideBattleMonsterUseCase: DecideBattleMonsterUseCase
+    lateinit var playerCellRepository: PlayerCellRepository
+    lateinit var startBattleUseCase: StartBattleUseCase
+
+    private val startNormalBattleUseCase: StartNormalBattleUseCase
+        get() = StartNormalBattleUseCaseImpl(
+            decideBattleMonsterUseCase = decideBattleMonsterUseCase,
+            playerCellRepository = playerCellRepository,
+            startBattleUseCase = startBattleUseCase,
+            restartUseCase = restartUseCase,
+            textRepository = textRepository
+        )
+
+    @Test
+    fun notStart() {
+        decideBattleMonsterUseCase = object : DecideBattleMonsterUseCase {
+            override fun invoke(backgroundCell: BackgroundCell): List<MonsterStatus> {
+                throw NotImplementedError()
+            }
+        }
+
+        playerCellRepository = object : PlayerCellRepository {
+            override val playerIncludeCellFlow: StateFlow<BackgroundCell?>
+                get() = throw NotImplementedError()
+            override var playerIncludeCell: BackgroundCell?
+                get() = throw NotImplementedError()
+                set(@Suppress("UNUSED_PARAMETER") value) {}
+            override var playerCenterCell: BackgroundCell
+                get() = BackgroundCell(
+                    cellType = CellType.Town1O,
+                    mapPoint = MapPoint(0, 0),
+                    square = NormalSquare(x = 0f, y = 0f, size = 0f)
+                )
+                set(@Suppress("UNUSED_PARAMETER") value) {}
+            override val eventCell: BackgroundCell
+                get() = throw NotImplementedError()
+        }
+
+        startBattleUseCase = object : StartBattleUseCase {
+            override fun invoke(
+                monsterList: List<MonsterStatus>,
+                battleEventCallback: BattleEventCallback,
+                backgroundType: BattleBackgroundType
+            ) {
+                NotImplementedError()
+            }
+        }
+
+        startNormalBattleUseCase.invoke()
+
+        // エラーが起こらなければOK
+    }
+
+
+    @Test
+    fun checkMonster() {
+        val cellType = CellType.Road
+        val expectedBackgroundCell = BackgroundCell(
+            cellType = cellType,
+            mapPoint = MapPoint(0, 0),
+            square = NormalSquare(x = 0f, y = 0f, size = 0f)
+        )
+        var count = 0
+
+        playerCellRepository = object : PlayerCellRepository {
+            override val playerIncludeCellFlow: StateFlow<BackgroundCell?>
+                get() = throw NotImplementedError()
+            override var playerIncludeCell: BackgroundCell?
+                get() = throw NotImplementedError()
+                set(@Suppress("UNUSED_PARAMETER") value) {}
+            override var playerCenterCell: BackgroundCell
+                get() = expectedBackgroundCell
+                set(@Suppress("UNUSED_PARAMETER") value) {}
+            override val eventCell: BackgroundCell
+                get() = throw NotImplementedError()
+        }
+
+        decideBattleMonsterUseCase = object : DecideBattleMonsterUseCase {
+            override fun invoke(backgroundCell: BackgroundCell): List<MonsterStatus> {
+                assertEquals(
+                    expected = expectedBackgroundCell,
+                    actual = backgroundCell,
+                )
+                count++
+                return listOf()
+            }
+        }
+
+        startBattleUseCase = object : StartBattleUseCase {
+            override fun invoke(
+                monsterList: List<MonsterStatus>,
+                battleEventCallback: BattleEventCallback,
+                backgroundType: BattleBackgroundType
+            ) {
+
+            }
+        }
+
+
+        startNormalBattleUseCase.invoke()
+
+        assertEquals(
+            expected = 1,
+            actual = count,
+        )
+    }
+
+    @Test
+    fun checkStart() {
+        val cellType = CellType.Road
+        val expectedBackgroundCell = BackgroundCell(
+            cellType = cellType,
+            mapPoint = MapPoint(0, 0),
+            square = NormalSquare(x = 0f, y = 0f, size = 0f)
+        )
+
+        val expectedBackground = cellType.toBattleBackGround()
+        val expectedMonsterList = listOf(
+            TestActiveMonster,
+            TestActiveMonster,
+        )
+        var count = 0
+
+        playerCellRepository = object : PlayerCellRepository {
+            override val playerIncludeCellFlow: StateFlow<BackgroundCell?>
+                get() = throw NotImplementedError()
+            override var playerIncludeCell: BackgroundCell?
+                get() = throw NotImplementedError()
+                set(@Suppress("UNUSED_PARAMETER") value) {}
+            override var playerCenterCell: BackgroundCell
+                get() = expectedBackgroundCell
+                set(@Suppress("UNUSED_PARAMETER") value) {}
+            override val eventCell: BackgroundCell
+                get() = throw NotImplementedError()
+        }
+
+        decideBattleMonsterUseCase = object : DecideBattleMonsterUseCase {
+            override fun invoke(backgroundCell: BackgroundCell): List<MonsterStatus> {
+                return expectedMonsterList
+            }
+        }
+
+        startBattleUseCase = object : StartBattleUseCase {
+            override fun invoke(
+                monsterList: List<MonsterStatus>,
+                battleEventCallback: BattleEventCallback,
+                backgroundType: BattleBackgroundType
+            ) {
+                assertEquals(
+                    expected = expectedMonsterList,
+                    actual = monsterList,
+                )
+                assertEquals(
+                    expected = expectedBackground,
+                    actual = backgroundType,
+                )
+
+                count++
+            }
+        }
+
+        startNormalBattleUseCase.invoke()
+
+        assertEquals(
+            expected = 1,
+            actual = count,
+        )
+    }
+
+}
