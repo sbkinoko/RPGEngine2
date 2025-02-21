36237b91c56b77f41feb9ee096b5cad29372b84a
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-02-21 18:57:58 +0900

    敵の攻撃のテスト作成

--- composeApp/src/commonTest/kotlin/gamescreen/battle/usecase/attack/AttackUseCaseImplFromEnemyTest.kt
+++ composeApp/src/commonTest/kotlin/gamescreen/battle/usecase/attack/AttackUseCaseImplFromEnemyTest.kt
@@ -1,7 +1,13 @@
 package gamescreen.battle.usecase.attack
 
+import core.ModuleCore
+import core.domain.status.PlayerStatusTest.Companion.testActivePlayer
+import core.domain.status.param.HP
+import core.repository.player.PlayerStatusRepository
+import data.ModuleData
 import gamescreen.battle.ModuleBattle
 import gamescreen.battle.QualifierAttackFromEnemy
+import kotlinx.coroutines.runBlocking
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.core.qualifier.named
@@ -9,10 +15,20 @@ import org.koin.test.KoinTest
 import org.koin.test.inject
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class AttackUseCaseImplFromEnemyTest : KoinTest {
     private val attackUseCaseImplFromEnemy: AttackUseCase by inject(
         qualifier = named(QualifierAttackFromEnemy)
+    )
+
+    private val playerStatusRepository: PlayerStatusRepository by inject()
+
+    private val hpValue = 50
+    val hp = HP(
+        value = hpValue,
+        maxValue = 100
     )
 
     @BeforeTest
@@ -20,6 +36,8 @@ class AttackUseCaseImplFromEnemyTest : KoinTest {
         startKoin {
             modules(
                 ModuleBattle,
+                ModuleCore,
+                ModuleData,
             )
         }
     }
@@ -27,5 +45,91 @@ class AttackUseCaseImplFromEnemyTest : KoinTest {
     @AfterTest
     fun afterTest() {
         stopKoin()
+    }
+
+    @Test
+    fun toActive() {
+        runBlocking {
+            playerStatusRepository.setStatus(
+                id = 0,
+                status = testActivePlayer.copy(
+                    hp = hp
+                ),
+            )
+            val damage = 5
+            attackUseCaseImplFromEnemy.invoke(
+                target = 0,
+                damage = damage,
+            )
+
+            playerStatusRepository.getStatus(0).apply {
+                assertEquals(
+                    expected = hpValue - damage,
+                    actual = this.hp.value
+                )
+            }
+        }
+    }
+
+    @Test
+    fun toActive2() {
+        val id = 1
+        runBlocking {
+            playerStatusRepository.setStatus(
+                id = id,
+                status = testActivePlayer.copy(
+                    hp = hp
+                ),
+            )
+            val damage = 5
+            attackUseCaseImplFromEnemy.invoke(
+                target = id,
+                damage = damage,
+            )
+
+            playerStatusRepository.getStatus(id).apply {
+                assertEquals(
+                    expected = hpValue - damage,
+                    actual = this.hp.value
+                )
+            }
+        }
+    }
+
+    @Test
+    fun toNotActive2() {
+        val idNotActive = 0
+        val idActive = 1
+        runBlocking {
+            playerStatusRepository.setStatus(
+                id = idNotActive,
+                status = testActivePlayer.copy(
+                    hp = HP(
+                        value = 0,
+                        maxValue = 100
+                    ),
+                ),
+            )
+            playerStatusRepository.setStatus(
+                id = idActive,
+                status = testActivePlayer.copy(
+                    hp = hp,
+                ),
+            )
+
+            playerStatusRepository
+            val damage = 5
+            attackUseCaseImplFromEnemy.invoke(
+                target = idNotActive,
+                damage = damage,
+            )
+
+            playerStatusRepository.getStatus(idActive).apply {
+                assertEquals(
+                    expected = hpValue - damage,
+                    actual = this.hp.value
+                )
+            }
+        }
     }
 }
