b9da46ad2c900e0cf4e5725beb41c6499df60738
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-02-22 20:29:36 +0900

    テストファイル作成

---
+++ composeApp/src/commonTest/kotlin/core/service/CheckCanUseServiceImplTest.kt
@@ -0,0 +1,28 @@
+package core.service
+
+import core.ModuleCore
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+
+class CheckCanUseServiceImplTest : KoinTest {
+    private val checkCanUseService: CheckCanUseService by inject()
+
+    @BeforeTest
+    fun beforeTest() {
+        startKoin {
+            modules(
+                ModuleCore,
+            )
+        }
+    }
+
+    @AfterTest
+    fun afterTest() {
+        stopKoin()
+    }
+
+}
