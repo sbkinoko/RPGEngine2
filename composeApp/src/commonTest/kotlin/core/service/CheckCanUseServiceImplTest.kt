a36237741aed225136fee81be19f385c043331b7
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-02-22 20:35:30 +0900

    テスト作成

--- composeApp/src/commonTest/kotlin/core/service/CheckCanUseServiceImplTest.kt
+++ composeApp/src/commonTest/kotlin/core/service/CheckCanUseServiceImplTest.kt
@@ -1,12 +1,17 @@
 package core.service
 
 import core.ModuleCore
+import core.domain.item.CostType
+import core.domain.status.PlayerStatusTest.Companion.testActivePlayer
+import core.domain.status.param.MP
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.test.KoinTest
 import org.koin.test.inject
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertFalse
 
 class CheckCanUseServiceImplTest : KoinTest {
     private val checkCanUseService: CheckCanUseService by inject()
@@ -25,4 +30,41 @@ class CheckCanUseServiceImplTest : KoinTest {
         stopKoin()
     }
 
+    @Test
+    fun canUseMp() {
+        val costMP = 5
+        val costType = CostType.MP(costMP)
+        val result = checkCanUseService.invoke(
+            status = testActivePlayer.copy(
+                mp = MP(
+                    value = costMP,
+                    maxValue = costMP,
+                )
+            ),
+            costType = costType,
+        )
+
+        assertFalse {
+            result
+        }
+    }
+
+    @Test
+    fun cannotUseMp() {
+        val costMP = 5
+        val costType = CostType.MP(costMP)
+        val result = checkCanUseService.invoke(
+            status = testActivePlayer.copy(
+                mp = MP(
+                    value = costMP - 1,
+                    maxValue = costMP,
+                )
+            ),
+            costType = costType,
+        )
+
+        assertFalse {
+            result
+        }
+    }
 }
