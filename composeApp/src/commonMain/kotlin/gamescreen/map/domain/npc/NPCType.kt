f88d01a0084c85702ed359517c36380ac5debf8d
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-02-28 20:09:14 +0900

    NPCの種類追加

--- composeApp/src/commonMain/kotlin/gamescreen/map/domain/npc/NPCType.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/map/domain/npc/NPCType.kt
@@ -4,4 +4,6 @@ enum class NPCType {
     GIRL,
     MARCHANT,
     ENEMY,
+    BOY,
+    TEMPLATE,
 }
--- composeApp/src/commonMain/kotlin/gamescreen/map/layout/npc/ImageBinder.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/map/layout/npc/ImageBinder.kt
@@ -4,6 +4,8 @@ import gamescreen.map.domain.npc.NPCType
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import rpgengine.composeapp.generated.resources.Res
+import rpgengine.composeapp.generated.resources.npc_
+import rpgengine.composeapp.generated.resources.npc_1
 import rpgengine.composeapp.generated.resources.npc_1_1_1
 import rpgengine.composeapp.generated.resources.npc_2
 import rpgengine.composeapp.generated.resources.npc_3
@@ -21,6 +23,8 @@ class ImageBinder {
             NPCType.GIRL -> Res.drawable.npc_1_1_1
             NPCType.MARCHANT -> Res.drawable.npc_2
             NPCType.ENEMY -> Res.drawable.npc_3
+            NPCType.BOY -> Res.drawable.npc_1
+            NPCType.TEMPLATE -> Res.drawable.npc_
         }
     }
 }
