e9720ceb0a0d944caeba46f0ecfce82305623954
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-04-18 19:08:03 +0900

    オブジェクトの真ん中は表示しない

--- composeApp/src/commonMain/kotlin/gamescreen/map/layout/background/ObjectData.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/map/layout/background/ObjectData.kt
@@ -1,16 +1,77 @@
 package gamescreen.map.layout.background
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import common.extension.pxToDp
 import gamescreen.map.domain.background.ObjectData
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
+import values.Colors
+
+class MapClip(
+    val width: Float,
+    val height: Float,
+) : Shape {
+    val path = Path().apply {
+        moveTo(0f, 0f)
+        lineTo(0f, height)
+        lineTo(width, height)
+        lineTo(width, 0f)
+        lineTo(0f, 0f)
+        moveTo(width, height / 2)
+        arcTo(
+            rect = Rect(
+                left = 0f,
+                right = width,
+                top = 0f,
+                bottom = height,
+            ),
+            startAngleDegrees = 0f,
+            sweepAngleDegrees = 180f,
+            forceMoveTo = false,
+        )
+        arcTo(
+            rect = Rect(
+                left = 0f,
+                right = width,
+                top = 0f,
+                bottom = height,
+            ),
+            startAngleDegrees = 180f,
+            sweepAngleDegrees = 180f,
+            forceMoveTo = false,
+        )
+
+        close()
+    }
+
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density,
+    ): Outline {
+        return Outline.Generic(path)
+    }
+}
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
@@ -20,8 +81,32 @@ fun ObjectData(
 ) {
     val imageBinder = ImageBinderBackground()
 
+    var width by remember {
+        mutableStateOf(0)
+    }
+    var height by remember {
+        mutableStateOf(0)
+    }
+
+
     // fixme 背景が動いてない場合はリロードしない
-    Box {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .onGloballyPositioned {
+                width = it.size.width
+                height = it.size.height
+            }
+            .clip(
+                shape = MapClip(
+                    width = width.toFloat(),
+                    height = height.toFloat(),
+                ),
+            )
+            .background(
+                color = Colors.Disabled,
+            )
+    ) {
         objectData.fieldData.forEach { backgroundCells ->
             backgroundCells.forEach inner@{ cell ->
                 if (cell == null) {
