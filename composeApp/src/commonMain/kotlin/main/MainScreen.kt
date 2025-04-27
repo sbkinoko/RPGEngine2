cc7123911f69b224e45fce517f30681b22220e17
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-04-27 16:52:08 +0900

    スタート画面からゲーム画面への遷移を作成

--- composeApp/src/commonMain/kotlin/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/main/MainScreen.kt
@@ -1,9 +1,26 @@
 package main
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import gamescreen.GameScreen
+import gamescreen.start.StartScreen
 
 @Composable
 fun MainScreen() {
-    GameScreen()
+    var screenType by remember {
+        mutableStateOf(
+            ScreenType.Start
+        )
+    }
+
+    when (screenType) {
+        ScreenType.Start -> StartScreen {
+            screenType = ScreenType.Game
+        }
+
+        ScreenType.Game -> GameScreen()
+    }
 }
