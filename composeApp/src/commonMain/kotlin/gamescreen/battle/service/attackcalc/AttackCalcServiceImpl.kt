90d43a8885d405ac90ad34df82d86ab6806baa90
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-05-27 14:49:15 +0900

    割合ダメージの処理作成

--- composeApp/src/commonMain/kotlin/gamescreen/battle/service/attackcalc/AttackCalcServiceImpl.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/battle/service/attackcalc/AttackCalcServiceImpl.kt
@@ -6,7 +6,6 @@ import core.domain.status.StatusData
 class AttackCalcServiceImpl : AttackCalcService {
     // todo ダメージのパターンを追加する
     // 固定ダメージ
-    // 割合ダメージ
     override fun invoke(
         attacker: StatusData,
         attacked: StatusData,
@@ -20,7 +19,9 @@ class AttackCalcServiceImpl : AttackCalcService {
                 )
             )
 
-            is DamageType.Rate -> TODO()
+            is DamageType.Rate -> attacked.decHP(
+                amount = attacked.hp.point * damageType.rate / 100
+            )
         }
     }
 }
