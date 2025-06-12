a634f0c0a1f0c548f3fbf968f6f42912c98e4a2f
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-06-12 15:54:39 +0900

    test修正

--- composeApp/src/commonTest/kotlin/main/viewmodel/MainViewModelTest.kt
+++ composeApp/src/commonTest/kotlin/main/viewmodel/MainViewModelTest.kt
@@ -1,6 +1,5 @@
 package main.viewmodel
 
-import main.MainViewModel
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.test.KoinTest
@@ -8,13 +7,11 @@ import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 
 class MainViewModelTest : KoinTest {
-    private lateinit var mainViewModel: MainViewModel
 
     @BeforeTest
     fun beforeTest() {
         startKoin {
         }
-        mainViewModel = MainViewModel()
     }
 
     @AfterTest
