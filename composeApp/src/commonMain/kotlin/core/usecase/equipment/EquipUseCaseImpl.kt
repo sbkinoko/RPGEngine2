235fd69c1bad54f281078254cda433499a95c2cd
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-06-24 15:55:13 +0900

    interface修正

--- composeApp/src/commonMain/kotlin/core/usecase/equipment/EquipUseCaseImpl.kt
+++ composeApp/src/commonMain/kotlin/core/usecase/equipment/EquipUseCaseImpl.kt
@@ -6,6 +6,7 @@ import core.domain.equipment.EquipmentType
 class EquipUseCaseImpl : EquipUseCase {
 
     override fun <T : EquipmentType> invoke(
+        target: Int,
         equipment: Equipment<T>,
     ) {
         TODO("Not yet implemented")
