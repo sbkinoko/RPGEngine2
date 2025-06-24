2fde66738c68befca826ae5de510350ccbfa0374
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-06-24 16:31:10 +0900

    引数追加

--- composeApp/src/commonMain/kotlin/core/usecase/equipment/EquipUseCaseImpl.kt
+++ composeApp/src/commonMain/kotlin/core/usecase/equipment/EquipUseCaseImpl.kt
@@ -1,8 +1,14 @@
 package core.usecase.equipment
 
 import core.domain.equipment.Equipment
+import core.domain.status.StatusType
+import core.repository.player.PlayerStatusRepository
+import core.repository.statusdata.StatusDataRepository
 
-class EquipUseCaseImpl : EquipUseCase {
+class EquipUseCaseImpl(
+    private val playerStatusRepository: PlayerStatusRepository,
+    private val statusDataRepository: StatusDataRepository<StatusType.Player>,
+) : EquipUseCase {
 
     override suspend fun invoke(
         target: Int,
