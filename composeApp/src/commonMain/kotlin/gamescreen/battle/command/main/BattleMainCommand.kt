c224f02d208ec7a93187f16f6f5cff5088af5160
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-06-30 15:57:04 +0900

    mainのリファクタ

--- composeApp/src/commonMain/kotlin/gamescreen/battle/command/main/BattleMainCommand.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/battle/command/main/BattleMainCommand.kt
@@ -1,15 +1,12 @@
 package gamescreen.battle.command.main
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import common.extension.equalAllocationModifier
-import common.extension.menuItem
-import common.layout.CenterText
+import gamescreen.battle.command.CommandMenu
 import org.koin.compose.koinInject
-import values.TextData
 
 @Composable
 fun BattleMainCommand(
@@ -17,33 +14,16 @@ fun BattleMainCommand(
     battleMainViewModel: BattleMainViewModel = koinInject(),
 ) {
     Column(modifier = modifier) {
-        Row(
-            modifier = equalAllocationModifier,
-        ) {
-            CenterText(
-                modifier = equalAllocationModifier
-                    .menuItem(
-                        id = BattleMainViewModel.ID_ATTACK,
-                        childViewModel = battleMainViewModel,
-                    ),
-                text = TextData.BATTLE_MAIN_ATTACK,
-            )
-
-            CenterText(
-                modifier = equalAllocationModifier
-                    .menuItem(
-                        id = BattleMainViewModel.ID_ESCAPE,
-                        childViewModel = battleMainViewModel,
-                    ),
-                text = TextData.BATTLE_MAIN_ESCAPE,
-            )
-        }
-        Row(
+        CommandMenu(
+            itemList = battleMainViewModel.entries,
+            modifier = Modifier.fillMaxWidth()
+                .weight(1f),
+            onClick2 = battleMainViewModel,
+        )
+        Spacer(
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f),
-        ) {
-
-        }
+        )
     }
 }
