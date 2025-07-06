66b44b9e97ecc6c797940c2265e13bd11850b4d6
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-07-06 18:05:46 +0900

    循環しないようにクラス削除

--- composeApp/src/commonMain/kotlin/data/battle/BattleDataRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/data/battle/BattleDataRepositoryImpl.kt
@@ -1,10 +1,8 @@
 package data.battle
 
-import core.domain.BattleEventCallback
 import data.event.EventManager
 import data.event.battle.BattleEventKey
 import data.monster.MonsterRepository
-import gamescreen.battle.domain.BattleBackgroundType
 import gamescreen.battle.domain.BattleId
 
 class BattleDataRepositoryImpl(
@@ -13,30 +11,13 @@ class BattleDataRepositoryImpl(
     private val eventManager: EventManager<BattleEventKey>,
 ) : BattleDataRepository {
     override fun getBattleMonsterData(battleId: BattleId): EventBattleData {
-        return when (battleId) {
-            BattleId.Battle1 -> EventBattleData(
-                monsterList = List(5) {
-                    monsterRepository.getMonster(0)
-                },
-                battleBackgroundType = BattleBackgroundType.Event,
-                battleEventCallback = BattleEventCallback(
-                    winCallback = {
-                        eventManager.callEvent(
-                            BattleEventKey.Win,
-                        )
-                    },
-                    escapeCallback = {
-                        eventManager.callEvent(
-                            BattleEventKey.Escape,
-                        )
-                    },
-                    loseCallback = {
-                        eventManager.callEvent(
-                            BattleEventKey.Lose,
-                        )
-                    },
-                ),
-            )
-        }
+        throw NotImplementedError()
+        //        return when (battleId) {
+        //            BattleId.Battle1 -> EventBattleData(
+        //                monsterList =,
+        //                battleBackgroundType =
+        //                battleEventCallback =
+        //            )
+        //        }
     }
 }
--- composeApp/src/commonMain/kotlin/data/event/battle/EventManagerImpl.kt
+++ composeApp/src/commonMain/kotlin/data/event/battle/EventManagerImpl.kt
@@ -1,63 +1,43 @@
 package data.event.battle
 
 import common.DefaultScope
+import core.domain.BattleEventCallback
 import core.usecase.heal.MaxHealUseCase
 import data.event.EventManager
-import gamescreen.battle.domain.BattleId
-import gamescreen.map.usecase.battleevent.StartEventBattleUseCase
+import data.monster.MonsterRepository
+import gamescreen.battle.domain.BattleBackgroundType
+import gamescreen.map.usecase.battlestart.StartBattleUseCase
 import gamescreen.text.TextBoxData
 import gamescreen.text.repository.TextRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
 
 class EventManagerImpl(
     private val textRepository: TextRepository,
     private val maxHealUseCase: MaxHealUseCase,
+
+    private val startBattleUseCase: StartBattleUseCase,
+    private val monsterRepository: MonsterRepository,
 ) : EventManager<BattleEventKey>, KoinComponent {
 
     private val mutableEventFlag = MutableStateFlow(0)
     override val eventFlag = mutableEventFlag.asStateFlow()
 
-    // todo 循環参照になるので設計を考える
-    private val startEventBattleUseCase: StartEventBattleUseCase by inject()
-
     override fun callEvent(
         key: BattleEventKey,
     ) {
-        when (key) {
-            BattleEventKey.Start -> {
-                val talkData = listOf(
-                    TextBoxData(
-                        text = if (eventFlag.value == 0) {
-                            "戦い"
-                        } else {
-                            "再戦"
-                        },
-                        callBack = {
-                            startEventBattleUseCase.invoke(
-                                battleId = BattleId.Battle1,
-                            )
-                        },
-                    ),
-                )
-                textRepository.push(
-                    talkData,
-                )
-            }
-
-            BattleEventKey.Win -> {
+        val battleCallBack = BattleEventCallback(
+            winCallback = {
                 textRepository.push(
                     textBoxData = TextBoxData(
                         text = "おめでとう",
                         callBack = { mutableEventFlag.value = 1 },
                     )
                 )
-            }
-
-            BattleEventKey.Lose -> {
+            },
+            escapeCallback = {
                 textRepository.push(
                     textBoxData = TextBoxData(
                         text = "また挑戦してね",
@@ -69,16 +49,38 @@ class EventManagerImpl(
                         }
                     ),
                 )
-            }
-
-            BattleEventKey.Escape -> {
+            },
+            loseCallback = {
                 textRepository.push(
                     textBoxData = TextBoxData(
                         text = "逃げるのも時には大事だね",
                         callBack = { mutableEventFlag.value = 1 },
                     )
                 )
-            }
-        }
+            },
+        )
+
+        val talkData = listOf(
+            TextBoxData(
+                text = if (eventFlag.value == 0) {
+                    "戦い"
+                } else {
+                    "再戦"
+                },
+                callBack = {
+                    startBattleUseCase.invoke(
+                        monsterList = List(5) {
+                            monsterRepository.getMonster(0)
+                        },
+                        backgroundType = BattleBackgroundType.Event,
+                        battleEventCallback = battleCallBack,
+
+                        )
+                },
+            ),
+        )
+        textRepository.push(
+            talkData,
+        )
     }
 }
