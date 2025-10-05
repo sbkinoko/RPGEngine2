ab33792288ced5da2f28e4c7351462a9ddd5f9e1
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-10-05 16:14:46 +0900

    返り値の型修正

--- composeApp/src/commonMain/kotlin/core/service/CheckCanUseServiceImpl.kt
+++ composeApp/src/commonMain/kotlin/core/service/CheckCanUseServiceImpl.kt
@@ -1,5 +1,6 @@
 package core.service
 
+import core.domain.AbleType
 import core.domain.item.CostType
 import core.domain.status.StatusData
 
@@ -7,17 +8,21 @@ class CheckCanUseServiceImpl : CheckCanUseService {
     override fun invoke(
         status: StatusData,
         costType: CostType,
-    ): Boolean {
+    ): AbleType {
         return when (costType) {
             is CostType.MP -> {
                 // 現在のMPがコストのMPより多ければ使用可能
-                status.mp.point >= costType.needMP
+                return if (status.mp.point >= costType.needMP) {
+                    AbleType.Able
+                } else {
+                    AbleType.CANT_USE_BY_MP
+                }
             }
 
             // ただの道具ならいつでも使える
             CostType.NotConsume,
             CostType.Consume,
-                -> true
+                -> AbleType.Able
         }
     }
 }
