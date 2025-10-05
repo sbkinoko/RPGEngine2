2c4d8df9199a739bcf35c2399ce8bd4504402a31
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-10-05 16:27:40 +0900

    test

--- composeApp/src/commonMain/kotlin/data/repository/item/skill/SkillId.kt
+++ composeApp/src/commonMain/kotlin/data/repository/item/skill/SkillId.kt
@@ -1,21 +1,16 @@
 package data.repository.item.skill
 
 
-// fixme enumにする
-sealed class SkillId {
-    data object NONE : SkillId()
-    data object CantUse : SkillId()
-
-    data object AttackToTwo : SkillId()
-    data object Normal1 : SkillId()
-    data object Normal2 : SkillId()
-
-    data object Heal : SkillId()
-    data object Revive : SkillId()
-
-    data object Paralysis : SkillId()
-    data object Poison : SkillId()
-
-    data object BufAtk : SkillId()
-    data object DeBufAtk : SkillId()
+enum class SkillId {
+    NONE,
+    CantUse,
+    AttackToTwo,
+    Normal1,
+    Normal2,
+    Heal,
+    Revive,
+    Paralysis,
+    Poison,
+    BufAtk,
+    DeBufAtk;
 }
