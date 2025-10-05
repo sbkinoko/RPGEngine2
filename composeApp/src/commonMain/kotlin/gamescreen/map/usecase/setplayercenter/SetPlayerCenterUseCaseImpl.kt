e922feda3040d6a7bb1d1a01d7977f6298efdcff
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-10-05 17:03:31 +0900

    余計な関数・file削除

--- composeApp/src/commonMain/kotlin/gamescreen/map/usecase/setplayercenter/SetPlayerCenterUseCaseImpl.kt
+++ composeApp/src/commonMain/kotlin/gamescreen/map/usecase/setplayercenter/SetPlayerCenterUseCaseImpl.kt
@@ -2,18 +2,15 @@ package gamescreen.map.usecase.setplayercenter
 
 import gamescreen.map.domain.Player
 import gamescreen.map.usecase.GetScreenCenterUseCase
-import gamescreen.map.usecase.PlayerMoveToUseCase
 
 class SetPlayerCenterUseCaseImpl(
     private val getScreenCenterUseCase: GetScreenCenterUseCase,
-    private val playerMoveToUseCase: PlayerMoveToUseCase,
 ) : SetPlayerCenterUseCase {
     override suspend fun invoke(
         player: Player,
     ): Player {
         val center = getScreenCenterUseCase.invoke()
-        return playerMoveToUseCase.invoke(
-            player = player,
+        return player.moveTo(
             x = center.x - player.size / 2,
             y = center.y - player.size / 2,
         )
