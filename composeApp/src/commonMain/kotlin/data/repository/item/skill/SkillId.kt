a10fbe8203b1b1c62e71bc4e2460246e0e7d5bb8
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-10-05 16:30:57 +0900

    改行追加

--- composeApp/src/commonMain/kotlin/data/repository/item/skill/SkillId.kt
+++ composeApp/src/commonMain/kotlin/data/repository/item/skill/SkillId.kt
@@ -4,13 +4,17 @@ package data.repository.item.skill
 enum class SkillId {
     NONE,
     CantUse,
+
     AttackToTwo,
     Normal1,
     Normal2,
+
     Heal,
     Revive,
+
     Paralysis,
     Poison,
+
     BufAtk,
     DeBufAtk;
 }
