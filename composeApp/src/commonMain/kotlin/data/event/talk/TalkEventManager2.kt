132f1526d84f085831bddda689da40c12b04d94e
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-11-24 21:28:03 +0900

    共通操作を抽象化

--- composeApp/src/commonMain/kotlin/data/event/talk/TalkEventManager2.kt
+++ composeApp/src/commonMain/kotlin/data/event/talk/TalkEventManager2.kt
@@ -1,61 +1,15 @@
 package data.event.talk
 
-import core.domain.realm.RealmEvent
-import data.event.EventManager
+import data.event.AbstractEventManager
 import gamescreen.text.TextBoxData
 import gamescreen.text.repository.TextRepository
-import io.realm.kotlin.Realm
-import io.realm.kotlin.RealmConfiguration
-import io.realm.kotlin.ext.query
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import values.event.EventGroup2
 
 class TalkEventManager2(
     private val textRepository: TextRepository,
-) : EventManager<EventGroup2> {
-
-    // todo 抽象クラスを作ってそこに持たせる
-    private val config: RealmConfiguration =
-        RealmConfiguration.create(schema = setOf(RealmEvent::class))
-    private val realm = Realm.open(config)
-
-    private val mutableStateFlow = MutableStateFlow(getData().flg)
-
-    override val eventFlag: StateFlow<Int>
-        get() = mutableStateFlow.asStateFlow()
-
-    // todo nameだけ実クラスで定義
-    // iniは抽象クラスに入れる
-    val ini = RealmEvent().apply {
-        name = "test"
-        flg = 0
-    }
-
-    // 抽象クラスに入れる
-    fun getData(): RealmEvent {
-        return realm.query<RealmEvent>("name == 'test'")
-            .first()
-            .find()
-            ?: run {
-                realm.writeBlocking {
-                    copyToRealm(ini)
-                }
-
-                ini
-            }
-    }
-
-    // todo 抽象クラスに入れる
-    fun updateFlg(flg: Int) {
-        mutableStateFlow.value = flg
-        realm.writeBlocking {
-            findLatest(getData())?.apply {
-                this.flg = flg
-            }
-        }
-    }
+) : AbstractEventManager<EventGroup2>() {
+    override val flgName: String
+        get() = "test"
 
     override fun callEvent(key: EventGroup2) {
         var talkData: List<TextBoxData>
